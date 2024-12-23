30f8116bfbe88124ba8ec45f564b7ae503a9216d
abdellah <Abdellah.chehri14@gmail.com>
2024-12-23 10:27:59 +0100

    feat: update the features section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-4xl font-bold">hi there this is my next js app</h1>
-      </div>
-    </>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
   );
 }
 
