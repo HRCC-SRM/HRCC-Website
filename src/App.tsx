d08212aeb27f2010dcc5453316e99a5f2e926604
abdellah <Abdellah.chehri14@gmail.com>
2024-12-23 09:12:31 +0100

    feat(landing page): update the font

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-4xl font-bold text-red-600">
-          hi there this is my next js app
-        </h1>
+        <h1 className="text-4xl font-bold">hi there this is my next js app</h1>
       </div>
     </>
   );
