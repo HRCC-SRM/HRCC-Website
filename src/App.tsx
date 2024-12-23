e7657fc12cd3421c7d525ed46506f82214d0da61
abdellah <Abdellah.chehri14@gmail.com>
2024-12-23 09:05:44 +0100

    setup tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-red-600">
+          hi there this is my next js app
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
