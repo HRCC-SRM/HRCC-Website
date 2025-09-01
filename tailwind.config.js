555556000f3f64caf44e1d0eb11c2e3850d9b8ec
Anik <anikdas210605@gmail.com>
2025-09-01 22:20:05 +0530

    fixed sizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: [
-    "object-[50%_30%]",
-  ],
   theme: {
     extend: {
       fontFamily: {
